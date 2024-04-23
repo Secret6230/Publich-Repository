d8563ba03d6f30244a36ca07e8baa251909a804f
Secret6230 <akinkale054@hotmail.com>
2024-04-23 12:17:24 +0200

    Warum liest du mich

--- Übung.py
+++ Übung.py
@@ -1,1 +1,14 @@
 print("Hallo")
+
+Schritt 0:
+Erstellt euch einen GitHub Account
+
+Schritt 1:
+erstellt ein repository auf GitHub als PUBLIC
+git clone (um das das repo lokal zu sehen in VSC)
+
+Schritt 2:
+erstellt eine Datei
+git add .
+git commit -m “”
+git push
